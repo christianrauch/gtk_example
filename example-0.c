a006cf94f0709707cd527608a6806b89385697b2
Christian Rauch <Rauch.Christian@gmx.de>
2020-11-13 01:00:08 +0000

    bla

--- example-0.c
+++ example-0.c
@@ -19,34 +19,36 @@ print_child(GtkWidget *widget, gpointer data)
   
   fflush(stdout);
   
-//  gtk_widget_render_icon_pixbuf()
-  ;
+//  gtk_widget_render_icon_pixbuf();
+  
+  gtk_widget_realize(widget);
 
   lvl++;
   if (GTK_IS_CONTAINER(widget)) {
     gtk_container_forall(GTK_CONTAINER(widget), &print_child, &lvl);
   }
   
-  if (GTK_IS_IMAGE(widget)) {
-      cairo_surface_t * srf = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 200, 200);
-      cairo_t *cr = cairo_create(srf);
-      gtk_widget_draw(widget, cr);
-      cairo_surface_write_to_png(srf, "/tmp/image.png");
-  }
+//  if (GTK_IS_IMAGE(widget)) {
+//      cairo_surface_t * srf = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 200, 200);
+//      cairo_t *cr = cairo_create(srf);
+//      gtk_widget_draw(widget, cr);
+//      cairo_surface_write_to_png(srf, "/tmp/image.png");
+//  }
   
-  if (GTK_IS_BUTTON(widget)) {
-    cairo_surface_t * srf = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 200, 200);
-    cairo_t *cr = cairo_create(srf);
-    gtk_widget_draw(widget, cr);
-    cairo_surface_write_to_png(srf, "/tmp/button.png");
-  }
+//  if (GTK_IS_BUTTON(widget)) {
+//    cairo_surface_t * srf = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 200, 200);
+//    cairo_t *cr = cairo_create(srf);
+//    gtk_widget_draw(widget, cr);
+//    cairo_surface_write_to_png(srf, "/tmp/button.png");
+//  }
 }
 
-//gboolean
-//draw(GtkWidget    *widget, cairo_t *cr, gpointer      user_data)
-//{
-//  //
-//}
+gboolean
+draw(GtkWidget    *widget, cairo_t *cr, gpointer      user_data)
+{
+  gtk_widget_draw(widget, cr);
+  return TRUE;
+}
 
 static void
 activate (GtkApplication* app,
@@ -61,28 +63,39 @@ activate (GtkApplication* app,
   gtk_window_set_titlebar((GtkWindow*)window, header);
   gtk_header_bar_set_show_close_button((GtkHeaderBar*)header, TRUE);
 
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
+//  gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 300, 200);
   gtk_widget_show_all (window);
 
-//  GList *children = gtk_container_get_children(GTK_CONTAINER(header));
+//  GList *children = gtk_container_get_children(GTK_CONTAINER(window));
 //  printf("children %i\n", g_list_length(children)); fflush(stdout);
-////  while ((children = g_list_next(children)) != NULL) {
-////      printf("child %s\n", gtk_widget_get_name((GtkWidget*)children)); fflush(stdout);
-////  }
+//  while ((children = g_list_next(children)) != NULL) {
+//      printf("child %s\n", gtk_widget_get_name((GtkWidget*)children)); fflush(stdout);
+//  }
 
 //  gtk_container_forall(GTK_CONTAINER(header), &container_cb, NULL);
   
-//  g_signal_connect(header, "draw", G_CALLBACK(draw), NULL);
+  g_signal_connect(window, "draw", G_CALLBACK(draw), NULL);
   
-  const int lvl = 0;
-  print_child(header, (void*)&lvl);
+//  const int lvl = 0;
+//  print_child(window, (void*)&lvl);
+//  gtk_widget_realize(header);
+  
+//  gtk_widget_realize(window);
+  
+//  cairo_surface_t * srf_offscreen = gtk_offscreen_window_get_surface(window);
+//  cairo_surface_write_to_png(srf_offscreen, "/tmp/offscreen.png");
+  
+  GdkEventButton ev;
+  gtk_main_do_event((GdkEvent*)&ev);
 
-  // cairo_surface_t * srf = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 200, 200);
-  // cairo_t *cr = cairo_create(srf);
-  // gtk_widget_draw(header, cr);
-  // // gtk_widget_draw(window, cr);
-  // cairo_surface_write_to_png(srf, "/tmp/win.png");
+   cairo_surface_t * srf = cairo_image_surface_create (
+         CAIRO_FORMAT_ARGB32,
+         gtk_widget_get_allocated_width(window),
+         gtk_widget_get_allocated_height(window));
+   cairo_t *cr = cairo_create(srf);
+   gtk_widget_draw(window, cr);
+   cairo_surface_write_to_png(srf, "/tmp/win.png");
 }
 
 int
